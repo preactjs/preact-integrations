9e9e1f27b8ba85fd0bdf370690dbe17491f57b24
Andre Wiggins <andrewiggins@live.com>
2019-09-12 18:05:52 -0700

    Add initial babel config

---
+++ babel.config.js
@@ -0,0 +1,20 @@
+module.exports = function(api) {
+  api.cache(true);
+
+  const presets = [
+    [
+      "@babel/preset-react",
+      {
+        pragma: "createElement",
+        pragmaFrag: "Fragment"
+      }
+    ]
+  ];
+
+  const plugins = [];
+
+  return {
+    presets,
+    plugins
+  };
+};
---
+++ src/index.js
@@ -0,0 +1,5 @@
+import { createElement } from "preact";
+
+export function App() {
+    return <div>Hello World!</div>;
+}
