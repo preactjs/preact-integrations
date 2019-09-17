30b01863fb2f03847129957d6018dfc072d56a1e
Andre Wiggins <andrewiggins@live.com>
2019-09-16 23:23:11 -0700

    Add basic implementation for preact-router

---
+++ src/direct/preact-router/index.js
@@ -0,0 +1,37 @@
+import { createElement } from "preact";
+import { Router, Link } from "preact-router";
+
+function Home() {
+	return (
+		<div>
+			<h1>Home</h1>
+		</div>
+	);
+}
+
+function About() {
+	return (
+		<div>
+			<h2>About</h2>
+		</div>
+	);
+}
+
+export default function App() {
+	return (
+		<div>
+			<nav>
+				<Link activeClassName="active" href="/">
+					Home
+				</Link>
+				<Link activeClassName="active" href="/about">
+					About
+				</Link>
+			</nav>
+			<Router>
+				<Home path="/" />
+				<About path="/about" />
+			</Router>
+		</div>
+	);
+}
