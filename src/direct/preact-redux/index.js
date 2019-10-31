eb34ebb62677fb7bc354c755e430983cdc7d9952
Andre Wiggins <andrewiggins@live.com>
2019-10-31 12:11:54 -0700

    Add preact-redux integration

--- src/direct/preact-redux/index.js
+++ src/direct/preact-redux/index.js
@@ -1,5 +1,80 @@
-import { createElement } from "preact";
+import { createStore } from "redux";
+import { Provider, connect } from "preact-redux";
+import { createElement, Component } from "preact";
 
-export default function App() {
-	return <h1>Preact Redux</h1>;
+const ADD_TODO = "ADD_TODO";
+const REMOVE_TODO = "REMOVE_TODO";
+
+const store = createStore((state = [], action) => {
+	switch (action.type) {
+		case ADD_TODO:
+			return [...state, action.payload];
+		case REMOVE_TODO:
+			return state.filter(todo => todo != action.payload);
+		default:
+			return state;
+	}
+});
+
+function mapStateToProps(state) {
+	return { todos: state };
+}
+
+function mapDispatchToProps(dispatch) {
+	return {
+		addTodo(content) {
+			dispatch({
+				type: ADD_TODO,
+				payload: content
+			});
+		},
+		removeTodo(content) {
+			dispatch({
+				type: REMOVE_TODO,
+				payload: content
+			});
+		}
+	};
+}
+
+class TodoApp extends Component {
+	state = { value: "" };
+
+	setValue = e => {
+		this.setState({ value: e.target.value });
+	};
+
+	addTodo = () => {
+		this.props.addTodo(this.state.value);
+		this.setState({ value: "" });
+	};
+
+	render() {
+		return (
+			<div>
+				<form onSubmit={this.addTodo} action="javascript:">
+					<input value={this.state.value} onInput={this.setValue} />
+					<button type="submit">Add Todo</button>
+				</form>
+				<ul>
+					{this.props.todos.map(todo => (
+						<li>{todo}</li>
+					))}
+				</ul>
+			</div>
+		);
+	}
+}
+
+const TodoConnected = connect(
+	mapStateToProps,
+	mapDispatchToProps
+)(TodoApp);
+
+export default function() {
+	return (
+		<Provider store={store}>
+			<TodoConnected />
+		</Provider>
+	);
 }
